6b78f2087b665cb396a8b09db7111fa52029c9ea
xxulin <497120555@qq.com>
2021-10-21 18:40:28 +0800

    ws修改为spot交易

--- src/main/java/com/binance/client/constant/BinanceApiConstants.java
+++ src/main/java/com/binance/client/constant/BinanceApiConstants.java
@@ -15,7 +15,7 @@ public class BinanceApiConstants {
     /**
      * Streaming API base URL.
      */
-    public static final String WS_API_BASE_URL = "wss://fstream.binance.com/ws";
+    public static final String WS_API_BASE_URL = "wss://stream.binance.com/ws";
 
     /**
      * HTTP Header to be used for API-KEY authentication.
